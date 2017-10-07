5f9ba57ac18974e2269a0197ebf42d7c2bcb44f8
Shreya Reddy <reddy.shreya18@gmail.com>
2017-10-07 02:27:02 -0700

    Created player class

---
+++ Spyfall/app/src/main/java/com/spyfall/android/model/Player.java
@@ -0,0 +1,18 @@
+package com.spyfall.android.model;
+
+public class Player {
+
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
