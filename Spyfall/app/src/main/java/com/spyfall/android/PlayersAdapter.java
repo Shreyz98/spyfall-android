aa9180e87c2edb34bcda55d3a5055b391588d644
Shreya Reddy <reddy.shreya18@gmail.com>
2017-10-07 18:48:17 -0700

    Started working on recycler view adapter

---
+++ Spyfall/app/src/main/java/com/spyfall/android/PlayersAdapter.java
@@ -0,0 +1,44 @@
+package com.spyfall.android;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.spyfall.android.model.Player;
+
+import java.util.ArrayList;
+
+public class PlayersAdapter extends RecyclerView.Adapter<PlayersAdapter.CustomViewHolder> {
+
+    private Context context;
+    private ArrayList<String> players;
+
+    PlayersAdapter(Context context, ArrayList<String> players) {
+        this.context = context;
+        this.players = players;
+    }
+
+    @Override
+    public CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+    }
+
+    @Override
+    public void onBindViewHolder(CustomViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    class CustomViewHolder extends RecyclerView.ViewHolder {
+
+
+        CustomViewHolder(View view) {
+            super(view);
+        }
+    }
+}
--- Spyfall/app/src/main/java/com/spyfall/android/RoomActivity.java
+++ Spyfall/app/src/main/java/com/spyfall/android/RoomActivity.java
@@ -2,6 +2,8 @@ package com.spyfall.android;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 
 public class RoomActivity extends AppCompatActivity {
@@ -9,7 +11,12 @@ public class RoomActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_room);
+
+        TextView accessKeyTitle = (TextView) findViewById(R.id.access_key_title);
+        TextView accessKey = (TextView) findViewById(R.id.access_key);
+        TextView playersTitle = (TextView) findViewById(R.id.players_title);
+
+        RecyclerView playersRV = (RecyclerView) findViewById(R.id.player_recycler_view);
     }
 }
